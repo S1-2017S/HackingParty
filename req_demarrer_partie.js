5deb0c5ce61600223ca689cef46041eb54eab61f
LECLERC <LECLERC@intechS1.fr>
2017-11-20 14:43:31 +0100

    creation de la requête de début de jeu + correction sur la page d'accueil

---
+++ req_demarrer_partie.js
@@ -0,0 +1,27 @@
+//=========================================================================
+//Traitement de "req_demarrer_partie"
+//Auteur : T. Leclerc
+//Version : 20/11/2017
+//=========================================================================
+"use strict";
+
+var fs = require("fs");
+reuire('remedial');
+
+var trait = function (req, res, query){
+
+	var page;
+
+	// AFFICHAGE DE LA modele_jeu
+
+	page = fs.readFileSync('modele_jeu.html', 'utf-8');
+
+	res.writeHead(200, {'Content-Type': 'text/html'});
+	res.write(page);
+	res.end();
+
+}
+
+
+//-----------------------------------------------------------------------------
+module.exports = trait;
