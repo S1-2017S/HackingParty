6b86791cbad570910d892b878ebd097d2265a02e
LECLERC <LECLERC@intechS1.fr>
2017-11-21 11:11:13 +0100

    création de la requête proposer

---
+++ req_proposer.js
@@ -0,0 +1,39 @@
+//============================================================================
+//Traitement de "req_proposer"
+//Auteur : groupe HackingParty
+//Version : 21/11/2017
+//===========================================================================
+
+"use strict";
+
+var fs = require("fs");
+require('remedial');
+
+var trait = function (req, res, query){
+	
+	var game_data = fs.readFileSync("./jeu.json", "UTF-8");
+	game_data = JSON.parse(game_data);
+	
+	var couleurs_joueur = [];
+
+	couleurs_joueur[0] = query.couleur1;
+	couleurs_joueur[1] = query.couleur2;
+	couleurs_joueur[2] = query.couleur3;
+	couleurs_joueur[3] = query.couleur4;
+
+	
+
+	page = fs.readFileSync('modele_jeu.html', 'utf-8');
+
+
+	res.writeHead(200, {'Content-Type': 'text/html'});
+	res.write(page);
+	res.end();
+
+}
+
+//---------------------------------------------------------------------
+
+module.exports = trait;
+
+trait ("a","a","a");
