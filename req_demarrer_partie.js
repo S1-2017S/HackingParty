57b9af1a0b7abd4bce37d225fb036bba01e35fef
LIENARD <LIENARD@intechS1.fr>
2017-11-28 09:48:09 +0100

    marqueur 5678

--- req_demarrer_partie.js
+++ req_demarrer_partie.js
@@ -32,6 +32,10 @@ var trait = function (req, res, query){
 	ligne1.marqueur21 = " ";
 	ligne1.marqueur31 = " ";
 	ligne1.marqueur41 = " ";
+	ligne1.marqueur51 = " ";
+	ligne1.marqueur61 = " ";
+	ligne1.marqueur71 = " ";
+	ligne1.marqueur81 = " ";
 
 	var ligne2 = {};
 
@@ -39,13 +43,21 @@ var trait = function (req, res, query){
 	ligne2.marqueur22 = " ";
 	ligne2.marqueur32 = " ";
 	ligne2.marqueur42 = " ";
-	
+	ligne2.marqueur52 = " ";
+	ligne2.marqueur62 = " ";
+	ligne2.marqueur72 = " ";
+	ligne2.marqueur82 = " ";
+
 	var ligne3 = {};
 
 	ligne3.marqueur13 = " ";
 	ligne3.marqueur23 = " ";
 	ligne3.marqueur33 = " ";
 	ligne3.marqueur43 = " ";
+	ligne3.marqueur53 = " ";
+	ligne3.marqueur63 = " ";
+	ligne3.marqueur73 = " ";
+	ligne3.marqueur83 = " ";
 
 	var ligne4 = {};
 
@@ -53,6 +65,10 @@ var trait = function (req, res, query){
 	ligne4.marqueur24 = " ";
 	ligne4.marqueur34 = " ";
 	ligne4.marqueur44 = " ";
+	ligne4.marqueur54 = " ";
+	ligne4.marqueur64 = " ";
+	ligne4.marqueur74 = " ";
+	ligne4.marqueur84 = " ";
 
 	var ligne5 = {};
 
@@ -60,6 +76,10 @@ var trait = function (req, res, query){
 	ligne5.marqueur25 = " ";
 	ligne5.marqueur35 = " ";
 	ligne5.marqueur45 = " ";
+	ligne5.marqueur55 = " ";
+	ligne5.marqueur65 = " ";
+	ligne5.marqueur75 = " ";
+	ligne5.marqueur85 = " ";
 
 	var ligne6 = {};
 
@@ -67,6 +87,10 @@ var trait = function (req, res, query){
 	ligne6.marqueur26 = " ";
 	ligne6.marqueur36 = " ";
 	ligne6.marqueur46 = " ";
+	ligne6.marqueur56 = " ";
+	ligne6.marqueur66 = " ";
+	ligne6.marqueur76 = " ";
+	ligne6.marqueur86 = " ";
 
 	var ligne7 = {};
 
@@ -74,20 +98,32 @@ var trait = function (req, res, query){
 	ligne7.marqueur27 = " ";
 	ligne7.marqueur37 = " ";
 	ligne7.marqueur47 = " ";
-
+	ligne7.marqueur57 = " ";
+	ligne7.marqueur67 = " ";
+	ligne7.marqueur77 = " ";
+	ligne7.marqueur87 = " ";
+	
 	var ligne8 = {};
 
 	ligne8.marqueur18 = " ";
 	ligne8.marqueur28 = " ";
 	ligne8.marqueur38 = " ";
 	ligne8.marqueur48 = " ";
-
+	ligne8.marqueur58 = " ";
+	ligne8.marqueur68 = " ";
+	ligne8.marqueur78 = " ";
+	ligne8.marqueur88 = " ";
+	
 	var ligne9 = {};
 
 	ligne9.marqueur19 = " ";
 	ligne9.marqueur29 = " ";
 	ligne9.marqueur39 = " ";
 	ligne9.marqueur49 = " ";
+	ligne9.marqueur59 = " ";
+	ligne9.marqueur69 = " ";
+	ligne9.marqueur79 = " ";
+	ligne9.marqueur89 = " ";
 
 	var ligne10 = {};
 
@@ -95,6 +131,10 @@ var trait = function (req, res, query){
 	ligne10.marqueur210 = " ":
 	ligne10.marqueur310 = " ";
 	ligne10.marqueur410 = " ";
+	ligne10.marqueur510 = " ";
+	ligne10.marqueur610 = " ";
+	ligne10.marqueur710 = " ";
+	ligne10.marqueur810 = " ";
 
 	var ligne11 = {};
 
@@ -102,6 +142,10 @@ var trait = function (req, res, query){
 	ligne11.marqueur211 = " ";
 	ligne11.marqueur311 = " ";
 	ligne11.marqueur411 = " ";
+	ligne11.marqueur511 = " ";
+	ligne11.marqueur611 = " ";
+	ligne11.marqueur711 = " ";
+	ligne11.marqueur811 = " ";
 
 	var ligne12 = {};
 
@@ -109,6 +153,11 @@ var trait = function (req, res, query){
 	ligne12.marqueur212 = " ";
 	ligne12.marqueur312 = " ";
 	ligne12.marqueur412 = " ";
+	ligne12.marqueur512 = " ";
+	ligne12.marqueur612 = " ";
+	ligne12.marqueur712 = " ";
+	ligne12.marqueur812 = " ";
+
 
 	tableau[0] = ligne1;
 	tableau[1] = ligne2;
