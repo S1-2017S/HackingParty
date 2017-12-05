d654d30e2e1a0087d19aeb9938be487f23176838
LOUBOUTIN <LOUBOUTIN@intechS1.fr>
2017-12-05 15:31:33 +0100

    req proposer qui marche + css

--- req_proposer.js
+++ req_proposer.js
@@ -20,42 +20,43 @@ var trait = function (req, res, query){
 	var tableau = game_data.tableau;
 	var ligne= tableau[game_data.essai];
 	var couleurs_joueur = [];
+	var tableau;
 
 	couleurs_joueur[0] = query.couleur1;
 	couleurs_joueur[1] = query.couleur2;
 	couleurs_joueur[2] = query.couleur3;
 	couleurs_joueur[3] = query.couleur4;
-	
+
 
 	var choix_pion_ordi = function (couleur, secret){
 		var i;
 		var j;
-		var col = [1,1,1,1];
+		var col = [0,0,0,0];
 		var test;
-		
+
 		for (i=0; i<4; i++){
 			if (couleur[i] === secret[i]){
 				col[i] = 2;
 			}
 		}
 
-		for (i=0; i<4 i++){
+		for (i=0; i<4; i++){
 			test = 0;
+
 			for (j=0; j<4; j++){
-				if (couleur[i] !== secret[j]){
+				/*if (couleur[i] !== secret[j]){
 					test++;
+				}*/
+				if (couleur[i] === secret[j] && col[j] !== 2 && col[i] !== 2){
+					col[i] = 1;
 				}
 			}
+			/*
 			if (test === 4){
 				col[i]=0;
-			}
+			}*/
 		}
-		/*
-		for (i=0; i<4; i++){
-			if (col[i] === 3){
-				col[i]=1;
-			}
-		}*/
+
 		return col;
 	}
 
@@ -104,37 +105,85 @@ var trait = function (req, res, query){
 			res.end();
 
 		}else{
-			col = choix_pion_ordi (couleurs_joueur, game_data.secret);
 			// AFFICHAGE DES CHOIX DU JOUEUR
 
 			if (game_data.essai === 0) {
-				
+				tableau = choix_pion_ordi (couleurs_joueur, game_data.secret);
 				ligne.marqueur11 = couleurs_joueur[0];
 				ligne.marqueur21 = couleurs_joueur[1];
 				ligne.marqueur31 = couleurs_joueur[2];
 				ligne.marqueur41 = couleurs_joueur[3];
 
+				if (tableau[0] === 0) {
+					ligne.marqueur51 = game_data.couleurs_ordi[0];
+				} else if (tableau[0] === 1){
+					ligne.marqueur51 = game_data.couleurs_ordi[1];
+				} else {
+					ligne.marqueur51 = game_data.couleurs_ordi[2];
+				}
+
+				if (tableau[1] === 0) {
+					ligne.marqueur61 = game_data.couleurs_ordi[0];
+				} else if (tableau[1] === 1){
+					ligne.marqueur61 = game_data.couleurs_ordi[1];
+				} else {
+					ligne.marqueur61 = game_data.couleurs_ordi[2];
+				}
+
+				if (tableau[2] === 0) {
+					ligne.marqueur71 = game_data.couleurs_ordi[0];
+				} else if (tableau[2] === 1) {
+					ligne.marqueur71 = game_data.couleurs_ordi[1];
+				} else {
+					ligne.marqueur71 = game_data.couleurs_ordi[2];
+				}
+
+				if (tableau[3] === 0) {
+					ligne.marqueur81 = game_data.couleurs_ordi[0];
+				} else if (tableau[3] === 1) {
+					ligne.marqueur81 = game_data.couleurs_ordi[1];
+				} else {
+					ligne.marqueur81 = game_data.couleurs_ordi[2];
+				}
 
 			} else if (game_data.essai === 1) {
 				ligne.marqueur12 = couleurs_joueur[0];
 				ligne.marqueur22 = couleurs_joueur[1];
 				ligne.marqueur32 = couleurs_joueur[2];
 				ligne.marqueur42 = couleurs_joueur[3];
+				tableau = choix_pion_ordi (couleurs_joueur, game_data.secret);
 
-				if (couleurs_joueur[0]===game_data.secret[0]){
-					ligne.marqueur52 = "noir";
-				}
-				if (couleurs_joueur[1]===game_data.secret[1]){
-					ligne.marqueur62 = "noir";
-				}
-				if (couleurs_joueur[2]===game_data.secret[2]){
-					ligne.marqueur72 = "noir";
-				}
-				if (couleurs_joueur[3]===game_data.secret[3]){
-					ligne.marqueur82 = "noir";
+				if (tableau[0] === 0) {
+					ligne.marqueur52 = game_data.couleurs_ordi[0];
+				} else if (tableau[0] === 1) {
+					ligne.marqueur52 = game_data.couleurs_ordi[1];
+				} else {
+					ligne.marqueur52 = game_data.couleurs_ordi[2];
 				}
 
+				if (tableau[1] === 0) {
+					ligne.marqueur62 = game_data.couleurs_ordi[0];
+				} else if (tableau[1] === 1) {
+					ligne.marqueur62 = game_data.couleurs_ordi[1];
+				} else {
+					ligne.marqueur62 = game_data.couleurs_ordi[2];
+				}
 
+				if (tableau[2] === 0) {
+					ligne.marqueur72 = game_data.couleurs_ordi[0];
+				} else if (tableau[2] === 1) {
+					ligne.marqueur72 = game_data.couleurs_ordi[1];
+				} else {
+					ligne.marqueur72 = game_data.couleurs_ordi[2];
+				}
+
+				if (tableau[3] === 0) {
+					ligne.marqueur82 = game_data.couleurs_ordi[0];
+				} else if (tableau[3] === 1) {
+					ligne.marqueur82 = game_data.couleurs_ordi[1];
+				} else {
+					ligne.marqueur82 = game_data.couleurs_ordi[2];
+				}
 
 
 			} else if (game_data.essai === 2) {
@@ -142,43 +191,78 @@ var trait = function (req, res, query){
 				ligne.marqueur23 = couleurs_joueur[1];
 				ligne.marqueur33 = couleurs_joueur[2];
 				ligne.marqueur43 = couleurs_joueur[3];
+				tableau = choix_pion_ordi (couleurs_joueur, game_data.secret);
 
-				if (couleurs_joueur[0]===game_data.secret[0]){
-					ligne.marqueur53 = "noir";
-				}
-				if (couleurs_joueur[1]===game_data.secret[1]){
-					ligne.marqueur63 = "noir";
-				}
-				if (couleurs_joueur[2]===game_data.secret[2]){
-					ligne.marqueur73 = "noir";
-				}
-				if (couleurs_joueur[3]===game_data.secret[3]){
-					ligne.marqueur83 = "noir";
+				if (tableau[0] === 0) {
+					ligne.marqueur53 = game_data.couleurs_ordi[0];
+				} else if (tableau[0] === 1) {
+					ligne.marqueur53 = game_data.couleurs_ordi[1];
+				} else { 
+					ligne.marqueur53 = game_data.couleurs_ordi[2];
 				}
 
+				if (tableau[1] === 0) {
+					ligne.marqueur63 = game_data.couleurs_ordi[0];
+				} else if (tableau[1] === 1) {
+					ligne.marqueur63 = game_data.couleurs_ordi[1];
+				} else { 
+					ligne.marqueur63 = game_data.couleurs_ordi[2];
+				}
 
+				if (tableau[2] === 0) {
+					ligne.marqueur73 = game_data.couleurs_ordi[0];
+				} else if (tableau[2] === 1) {
+					ligne.marqueur73 = game_data.couleurs_ordi[1];
+				} else {
+					ligne.marqueur73 = game_data.couleurs_ordi[2];
+				}
 
+				if (tableau[3] === 0) {
+					ligne.marqueur83 = game_data.couleurs_ordi[0];
+				} else if (tableau[3] === 1) {
+					ligne.marqueur83 = game_data.couleurs_ordi[1];
+				} else {
+					ligne.marqueur83 = game_data.couleurs_ordi[2];
+				}
 
 			} else if (game_data.essai === 3) {
 				ligne.marqueur14 = couleurs_joueur[0];
 				ligne.marqueur24 = couleurs_joueur[1];
 				ligne.marqueur34 = couleurs_joueur[2];
 				ligne.marqueur44 = couleurs_joueur[3];
+				tableau = choix_pion_ordi (couleurs_joueur, game_data.secret);
 
-				if (couleurs_joueur[0]===game_data.secret[0]){
-					ligne.marqueur54 = "noir";
-				}
-				if (couleurs_joueur[1]===game_data.secret[1]){
-					ligne.marqueur64 = "noir";
-				}
-				if (couleurs_joueur[2]===game_data.secret[2]){
-					ligne.marqueur74 = "noir";
-				}
-				if (couleurs_joueur[3]===game_data.secret[3]){
-					ligne.marqueur84 = "noir";
+				if (tableau[0] === 0) {
+					ligne.marqueur54 = game_data.couleurs_ordi[0];
+				} else if (tableau[0] === 1) {
+					ligne.marqueur54 = game_data.couleurs_ordi[1];
+				} else { 
+					ligne.marqueur54 = game_data.couleurs_ordi[2];
 				}
 
+				if (tableau[1] === 0) {
+					ligne.marqueur64 = game_data.couleurs_ordi[0];
+				} else if (tableau[1] === 1) {
+					ligne.marqueur64 = game_data.couleurs_ordi[1];
+				} else { 
+					ligne.marqueur64 = game_data.couleurs_ordi[2];
+				}
 
+				if (tableau[2] === 0) {
+					ligne.marqueur74 = game_data.couleurs_ordi[0];
+				} else if (tableau[2] === 1) {
+					ligne.marqueur74 = game_data.couleurs_ordi[1];
+				} else {
+					ligne.marqueur74 = game_data.couleurs_ordi[2];
+				}
+
+				if (tableau[3] === 0) {
+					ligne.marqueur84 = game_data.couleurs_ordi[0];
+				} else if (tableau[3] === 1) {
+					ligne.marqueur84 = game_data.couleurs_ordi[1];
+				} else {
+					ligne.marqueur84 = game_data.couleurs_ordi[2];
+				}
 
 
 			} else if (game_data.essai === 4) {
@@ -186,21 +270,39 @@ var trait = function (req, res, query){
 				ligne.marqueur25 = couleurs_joueur[1];
 				ligne.marqueur35 = couleurs_joueur[2];
 				ligne.marqueur45 = couleurs_joueur[3];
+				tableau = choix_pion_ordi (couleurs_joueur, game_data.secret);
 
-				if (couleurs_joueur[0]===game_data.secret[0]){
-					ligne.marqueur55 = "noir";
-				}
-				if (couleurs_joueur[1]===game_data.secret[1]){
-					ligne.marqueur65 = "noir";
-				}
-				if (couleurs_joueur[2]===game_data.secret[2]){
-					ligne.marqueur75 = "noir";
-				}
-				if (couleurs_joueur[3]===game_data.secret[3]){
-					ligne.marqueur85 = "noir";
+				if (tableau[0] === 0) {
+					ligne.marqueur55 = game_data.couleurs_ordi[0];
+				} else if (tableau[0] === 1) {
+					ligne.marqueur55 = game_data.couleurs_ordi[1];
+				} else { 
+					ligne.marqueur55 = game_data.couleurs_ordi[2];
 				}
 
+				if (tableau[1] === 0) {
+					ligne.marqueur65 = game_data.couleurs_ordi[0];
+				} else if (tableau[1] === 1) {
+					ligne.marqueur65 = game_data.couleurs_ordi[1];
+				} else { 
+					ligne.marqueur65 = game_data.couleurs_ordi[2];
+				}
 
+				if (tableau[2] === 0) {
+					ligne.marqueur75 = game_data.couleurs_ordi[0];
+				} else if (tableau[2] === 1) {
+					ligne.marqueur75 = game_data.couleurs_ordi[1];
+				} else {
+					ligne.marqueur75 = game_data.couleurs_ordi[2];
+				}
+
+				if (tableau[3] === 0) {
+					ligne.marqueur85 = game_data.couleurs_ordi[0];
+				} else if (tableau[3] === 1) {
+					ligne.marqueur85 = game_data.couleurs_ordi[1];
+				} else {
+					ligne.marqueur85 = game_data.couleurs_ordi[2];
+				}
 
 
 			} else if (game_data.essai === 5) {
@@ -208,21 +310,39 @@ var trait = function (req, res, query){
 				ligne.marqueur26 = couleurs_joueur[1];
 				ligne.marqueur36 = couleurs_joueur[2];
 				ligne.marqueur46 = couleurs_joueur[3];
+				tableau = choix_pion_ordi (couleurs_joueur, game_data.secret);
 
-
-				if (couleurs_joueur[0]===game_data.secret[0]){
-					ligne.marqueur56 = "noir";
-				}
-				if (couleurs_joueur[1]===game_data.secret[1]){
-					ligne.marqueur66 = "noir";
-				}
-				if (couleurs_joueur[2]===game_data.secret[2]){
-					ligne.marqueur76 = "noir";
-				}
-				if (couleurs_joueur[3]===game_data.secret[3]){
-					ligne.marqueur86 = "noir";
+				if (tableau[0] === 0) {
+					ligne.marqueur56 = game_data.couleurs_ordi[0];
+				} else if (tableau[0] === 1) {
+					ligne.marqueur56 = game_data.couleurs_ordi[1];
+				} else { 
+					ligne.marqueur56 = game_data.couleurs_ordi[2];
 				}
 
+				if (tableau[1] === 0) {
+					ligne.marqueur66 = game_data.couleurs_ordi[0];
+				} else if (tableau[1] === 1) {
+					ligne.marqueur66 = game_data.couleurs_ordi[1];
+				} else { 
+					ligne.marqueur66 = game_data.couleurs_ordi[2];
+				}
+
+				if (tableau[2] === 0) {
+					ligne.marqueur76 = game_data.couleurs_ordi[0];
+				} else if (tableau[2] === 1) {
+					ligne.marqueur76 = game_data.couleurs_ordi[1];
+				} else {
+					ligne.marqueur76 = game_data.couleurs_ordi[2];
+				}
+
+				if (tableau[3] === 0) {
+					ligne.marqueur86 = game_data.couleurs_ordi[0];
+				} else if (tableau[3] === 1) {
+					ligne.marqueur86 = game_data.couleurs_ordi[1];
+				} else {
+					ligne.marqueur86 = game_data.couleurs_ordi[2];
+				}
 
 
 
@@ -231,19 +351,38 @@ var trait = function (req, res, query){
 				ligne.marqueur27 = couleurs_joueur[1];
 				ligne.marqueur37 = couleurs_joueur[2];
 				ligne.marqueur47 = couleurs_joueur[3];
+				tableau = choix_pion_ordi (couleurs_joueur, game_data.secret);
 
+				if (tableau[0] === 0) {
+					ligne.marqueur57 = game_data.couleurs_ordi[0];
+				} else if (tableau[0] === 1) {
+					ligne.marqueur57 = game_data.couleurs_ordi[1];
+				} else { 
+					ligne.marqueur57 = game_data.couleurs_ordi[2];
+				}
 
-				if (couleurs_joueur[0]===game_data.secret[0]){
-					ligne.marqueur57 = "noir";
+				if (tableau[1] === 0) {
+					ligne.marqueur67 = game_data.couleurs_ordi[0];
+				} else if (tableau[1] === 1) {
+					ligne.marqueur67 = game_data.couleurs_ordi[1];
+				} else { 
+					ligne.marqueur67 = game_data.couleurs_ordi[2];
 				}
-				if (couleurs_joueur[1]===game_data.secret[1]){
-					ligne.marqueur67 = "noir";
+
+				if (tableau[2] === 0) {
+					ligne.marqueur77 = game_data.couleurs_ordi[0];
+				} else if (tableau[2] === 1) {
+					ligne.marqueur77 = game_data.couleurs_ordi[1];
+				} else {
+					ligne.marqueur77 = game_data.couleurs_ordi[2];
 				}
-				if (couleurs_joueur[2]===game_data.secret[2]){
-					ligne.marqueur77 = "noir";
-				}
-				if (couleurs_joueur[3]===game_data.secret[3]){
-					ligne.marqueur87 = "noir";
+
+				if (tableau[3] === 0) {
+					ligne.marqueur87 = game_data.couleurs_ordi[0];
+				} else if (tableau[3] === 1) {
+					ligne.marqueur87 = game_data.couleurs_ordi[1];
+				} else {
+					ligne.marqueur87 = game_data.couleurs_ordi[2];
 				}
 
 
